eeef9910fe72262130cada9a06df9abbd77c41f8
64070148NatthawutSornkhiew <89625074+64070148NatthawutSornkhiew@users.noreply.github.com>
2022-11-10 20:28:23 +0700

    done design home

--- index.php
+++ index.php
@@ -11,19 +11,18 @@
     <!---icon--->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
-    
         <?php include 'style.css'; ?>
     </style>
-</head>                
+</head>
 
 <body onload="retrieve(); count(); is_empty();">
-<?php include 'cart.php'; ?>
-<?php include 'nav.php'; ?>
-<?php include 'background.php'; ?>
+    <?php include 'cart.php'; ?>
+    <?php include 'nav.php'; ?>
+    <?php include 'background.php'; ?>
     <!--แทบ logo--------TH/EN--------deli-take-------selectAdd---------buy1get1-ValueSet------START---->
     <!-- navbar -->
     <!--แทบ logo--------TH/EN--------deli-take-------selectAdd---------buy1get1-ValueSet------START---->
-    
+
     <!-- โฆษณา -->
     <section id="sec1">
         <div id="ad-container">
@@ -69,72 +68,123 @@
             </div>
         </div>
     </section><br><br>
-    <br>
+    <br><br><br>
 
-<!-- recommend menu -->
-<div>
-    <h2 style="margin-left:8%; color:#271E17; background:#e3c596d6; border-radius: 20px; padding:5px; width:max-content; font-size:100%;">recommend menu</h2>
-    <div class="container">
-        <div class="row">
-            <!-- recommend 1 -->
-            <div class="col-sm">
-                <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
-                    <img class="card-img-top" src="./img/pasta/pasta_1.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h6 class="card-title"  id="name_recommend_menu">Egg Tart Hong Kong Style 2 Pcs.</h6>
-                        <h6 class="card-title" style="float: right;">129฿</h6><br>
-                        
-                        <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+    <!-- recommend menu -->
+    <div>
+        <div class="d-flex justify-content-center align-item-center">
+        <h2 style="margin-left:8%; color:white;  border-radius: 20px; padding:5px; width:max-content; font-size:28px;">Recommend Menu</h2>
+        </div><br>
+        <div class="container">
+            <div class="row">
+                <!-- recommend 1 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="./img/home/rec_1.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Chocolate Chip Ice Cream</h6>
+                            <h6 class="card-title" style="float: right;">219฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- recommend 2 -->
-            <div class="col-sm">
-                <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
-                    <img class="card-img-top" src="./img/cheese/4_Cheese_&_Bacon_l.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h6 class="card-title"  id="name_recommend_menu">Mocha Almond Fudge Ice Cream</h6>
-                        <h6 class="card-title" style="float: right;">219฿</h6><br>
-                        
-                        <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                <!-- recommend 2 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="./img/home/rec_2.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Caesar Salad</h6>
+                            <h6 class="card-title" style="float: right;">149฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- recommend 3 -->
-            <div class="col-sm">
-                <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
-                    <img class="card-img-top" src="img/Caesar_Salad.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h6 class="card-title"  id="name_recommend_menu">Caesar Salad</h6>
-                        <h6 class="card-title" style="float: right;">129฿</h6><br>
-                        
-                        <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                <!-- recommend 3 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="img/home/rec_3.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">BBQ Pork Ribs</h6>
+                            <h6 class="card-title" style="float: right;">359฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- recommend 4 -->
-            <div class="col-sm">
-                <div class="card" id="c_recommend_menu" style="width: 15rem; margin:auto; ">
-                    <img class="card-img-top" src="img/BBQ_Pork.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h6 class="card-title" id="name_recommend_menu">BBQ Pork Ribs</h6>
-                        <h6 class="card-title" style="float: right;">359฿</h6><br>
-                        <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                <!-- recommend 4 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem; margin:auto; ">
+                        <img class="card-img-top" src="img/home/rec_4.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Calamari</h6>
+                            <h6 class="card-title" style="float: right;">99฿</h6><br>
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
+                    </div>
+                </div>
+            </div><br>
+            <div class="row">
+                <!-- recommend 5 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="img/home/rec_5.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Chip & Cheese Dip</h6>
+                            <h6 class="card-title" style="float: right;">109฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- recommend 6 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="img/home/rec_6.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Cheese Sticks</h6>
+                            <h6 class="card-title" style="float: right;">89฿</h6><br>
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- recommend 7 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="img/home/rec_7.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Coke 510 ml.</h6>
+                            <h6 class="card-title" style="float: right;">24฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- recommend 8 -->
+                <div class="col-sm-3">
+                    <div class="card" id="c_recommend_menu" style="width: 15rem;  margin:auto;">
+                        <img class="card-img-top" src="img/home/rec_8.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="name_recommend_menu">Egg Tart 2 Pcs.</h6>
+                            <h6 class="card-title" style="float: right;">39฿</h6><br>
+
+                            <a href="#" class="btn btn-success" id="get_cart"><i class="bi bi-cart">&nbsp&nbspAdd to Cart</i></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<br>
-<div style="display: flex; justify-content: center;
+    <br><br>
+    <div style="display: flex; justify-content: center;
 align-items: center; ">
-    <img src="./img/home/soon.jpg" width="60%" style="background:#E3C696c3; border-radius: 20%;">
-    <h1 style="position:absolute;">coming soon</h1>
-</div>
-<br>
+        <img src="./img/home/soon.jpg" width="60%" style="background:#E3C696c3; border-radius: 20%;">
+        <h1 style="position:absolute;">coming soon</h1>
+    </div>
+    <br>
     <!-- footer -->
-    <?php include 'footer.php';?>
+    <?php include 'footer.php'; ?>
     <script src="pizza.js"></script>
 </body>
 
