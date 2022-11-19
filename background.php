e42420f451680030b5377c294e8f9d57b538fc8b
64070148NatthawutSornkhiew <89625074+64070148NatthawutSornkhiew@users.noreply.github.com>
2022-11-19 13:03:42 +0700

    done

--- background.php
+++ background.php
@@ -9,11 +9,11 @@
 </head>
 <body>
     <div id="background">
-        <img class="background-img" src="../Webtech-Project-2022/img/home/pizza.png" alt="pizza" style="top: 0; left: 10%; opacity:.6;">
-        <img class="background-img" src="../Webtech-Project-2022/img/home/fork.png" alt="pizza" style="top: 0; left: 70%; opacity:.6;">
-        <img class="background-img" src="../Webtech-Project-2022/img/home/bottle.png" alt="pizza" style="top: 50%; left: 50%; opacity:.6;">
-        <img class="background-img" src="../Webtech-Project-2022/img/home/chef.png" alt="pizza" style="top: 55%; left: -5%;">
-        <img class="background-img" src="../Webtech-Project-2022/img/home/pizza2.png" alt="pizza" style="top: 70%; left: 90%; opacity:.6;">
+        <img class="background-img" src="./img/home/pizza.png" alt="pizza" style="top: 0; left: 10%; opacity:.6;">
+        <img class="background-img" src="./img/home/fork.png" alt="pizza" style="top: 0; left: 70%; opacity:.6;">
+        <img class="background-img" src="./img/home/bottle.png" alt="pizza" style="top: 50%; left: 50%; opacity:.6;">
+        <img class="background-img" src="./img/home/chef.png" alt="pizza" style="top: 55%; left: -5%;">
+        <img class="background-img" src="./img/home/pizza2.png" alt="pizza" style="top: 70%; left: 90%; opacity:.6;">
     </div>
 </body>
 </html>
